1defd91e8547603354874dafd64519c87f0331be
syc <1746950509@qq.com>
2022-03-09 21:25:17 +0800

    update_image

--- openapi/urls.py
+++
@@ -1,7 +0,0 @@
-
-
-from django.urls import path
-from openapi import views
-urlpatterns = [
-    path('get_all_articels/', views.get_all_articels)
-]
--- openapi/views.py
+++
@@ -1,8 +0,0 @@
-import json
-
-from django.shortcuts import render
-
-# Create your views here.
-
-def get_all_articels(request):
-    return json.dumps({})
