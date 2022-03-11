8742228e39939cce26a8791e32c8c05f542d64b6
syc <1746950509@qq.com>
2022-03-11 21:20:04 +0800

    edit_urlConfig

--- openapi/urls.py
+++
@@ -1,9 +0,0 @@
-
-
-from django.urls import path
-from openapi import views
-urlpatterns = [
-    path('get_all_articels', views.get_all_articels),
-    path('sendMessage', views.send_mobile_msg),
-    path('getToken', views.getToken)
-]
